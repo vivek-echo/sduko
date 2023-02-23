a77d368e37b8bb1032e2f708c78585739a9093fd
Vishal Kumar <vishal.k@csm.tech>
2023-02-23 21:26:25 +0530

    changes

--- resources/views/InnerPannel/Dashboard/Dashboard.blade.php
+++ resources/views/InnerPannel/Dashboard/Dashboard.blade.php
@@ -2,7 +2,104 @@
 
 @section('content')
 
-svbsjhvbsbvbb
+<div class="row">
+    <div class="col-xl-3 col-xxl-4 col-lg-6 col-sm-6">
+        <div class="widget-stat card">
+            <div class="card-body p-4">
+                <div class="media ai-icon">
+                    <span class="me-3 bgl-primary text-primary">
+                        <i class="ti-layout-accordion-list"></i>
+                    </span>
+                    <div class="media-body">
+                        <p class="mb-1">Total Posts</p>
+                        <h4 class="mb-0">150</h4>
+                        <span class="badge badge-primary">+3.5%</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-3 col-xxl-4 col-lg-6 col-sm-6">
+        <div class="widget-stat card">
+            <div class="card-body p-4">
+                <div class="media ai-icon">
+                    <span class="me-3 bgl-warning text-warning">
+                        <i class="ti-layout-accordion-list"></i>
+                    </span>
+                    <div class="media-body">
+                        <p class="mb-1">Total Pending Posts</p>
+                        <h4 class="mb-0">150</h4>
+                        <span class="badge badge-warning">+3.5%</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-3 col-xxl-4 col-lg-6 col-sm-6">
+        <div class="widget-stat card">
+            <div class="card-body p-4">
+                <div class="media ai-icon">
+                    <span class="me-3 bgl-success text-success">
+                        <i class="ti-layout-accordion-list"></i>
+                    </span>
+                    <div class="media-body">
+                        <p class="mb-1">Total Approved Posts</p>
+                        <h4 class="mb-0">150</h4>
+                        <span class="badge badge-success">+3.5%</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-3 col-xxl-4 col-lg-6 col-sm-6">
+        <div class="widget-stat card">
+            <div class="card-body p-4">
+                <div class="media ai-icon">
+                    <span class="me-3 bgl-danger text-danger">
+                        <i class="ti-layout-accordion-list"></i>
+                    </span>
+                    <div class="media-body">
+                        <p class="mb-1">Total Rejected Posts</p>
+                        <h4 class="mb-0">150</h4>
+                        <span class="badge badge-danger">+3.5%</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-3 col-xxl-4 col-lg-6 col-sm-6">
+        <div class="widget-stat card">
+            <div class="card-body p-4">
+                <div class="media ai-icon">
+                    <span class="me-3 bgl-danger text-info">
+                        <i class="ti-user"></i>
+                    </span>
+                    <div class="media-body">
+                        <p class="mb-1">Total Users</p>
+                        <h4 class="mb-0">150</h4>
+                        <span class="badge badge-info">+3.5%</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-3 col-xxl-4 col-lg-6 col-sm-6">
+        <div class="widget-stat card">
+            <div class="card-body p-4">
+                <div class="media ai-icon">
+                    <span class="me-3 bgl-danger text-info">
+                        <i class="ti-star"></i>
+                    </span>
+                    <div class="media-body">
+                        <p class="mb-1">Total Models</p>
+                        <h4 class="mb-0">150</h4>
+                        <span class="badge badge-info">+3.5%</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
 <script>
   
